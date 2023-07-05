340311ea69d8f17ce9f1c9802a2b06960c1757cf
Chooongg <chooongg@outlook.com>
2023-07-05 17:42:26 +0800

    add SpanExt

--- utils/src/main/java/com/chooongg/utils/ChooonggUtilsInitializer.kt
+++ utils/src/main/java/com/chooongg/utils/ChooonggUtilsInitializer.kt
@@ -5,12 +5,10 @@ import android.content.Context
 import androidx.startup.Initializer
 import com.chooongg.utils.manager.ApplicationManager
 import com.chooongg.utils.manager.NightModeManager
-import com.tencent.mmkv.MMKV
 
 class ChooonggUtilsInitializer : Initializer<String> {
     override fun create(context: Context): String {
         ApplicationManager.initialize(context as Application)
-        MMKV.initialize(context)
         NightModeManager.initialize(context)
         return "ChooonggUtils is initialized"
     }
